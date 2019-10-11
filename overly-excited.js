29f678111f76e11cc26b9faf5d74704c9f5acb9a
Cherkesky <guy@cherkesky.com>
2019-10-11 13:17:35 -0500

    exercise completed and notations added

--- overly-excited.js
+++ overly-excited.js
@@ -18,26 +18,36 @@
 // The walrus danced through the trees in the light of the moon ...
 
 // Create an array that contains the words in the sentence
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 /*
     The addExcitement function should be an impure function, and accept
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement(theWordArray) {
 
-    // Each time the for loop executes, you're going to add one more word to this string
-    let buildMeUp = ""
+  // Each time the for loop executes, you're going to add one more word to this string
+  let buildMeUp = ""
+  let thirdWordCounter = 0;
 
-    for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp += sentence[i] += " ";
-        // Print buildMeUp to the console
-        console.log(buildMeUp);
+  for (let i = 0; i < theWordArray.length; i++) {
+    thirdWordCounter++;
+        // lets check if thirdWordCounter got to 3, and if so, lets add a print out of "!"
+    if (thirdWordCounter === 3) {
+      buildMeUp += sentence[i] += "!" + " ";
+      // lets reset thirdWordCounter to get ready for counting again to 3
+      thirdWordCounter = 0;
+    } else {
+      // Concatenate the new word onto buildMeUp
+      buildMeUp += sentence[i] += " ";
+      // Print buildMeUp to the console
+      console.log(buildMeUp);
     }
-
+  }
 }
 
 // Invoke the function and pass in the array
 addExcitement(sentence)
+
+
